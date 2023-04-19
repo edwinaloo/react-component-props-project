b5e975df2e2b86bbdd52e6e6fe5268f37a98cca2
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-04-20 00:17:15 +0300

    App.js and Header.js fixed.New components created

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
 import React from "react";
 import blogData from "../data/blog";
-
-console.log(blogData);
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header blogName={blogData.name} />
+      <p>{blogData.description}</p>
+      <h2>Recent Posts:</h2>
+      <ul>
+        {blogData.posts.map((post) => (
+          <li key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.content}</p>
+            <p>By {post.author}</p>
+            <p>{post.date}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
